31dba42bd811f88a1feb16243a446eddfd88e302
Kirasy42 <hanifanaktasik@gmail.com>
2025-01-17 15:31:59 +0700

    chore(auth.js): refactor auth method

--- uas-expressJS/middlewares/auth.js
+++ uas-expressJS/middlewares/auth.js
@@ -1,15 +1,23 @@
-// middlewares/auth.js
-const jwt = require('jsonwebtoken');
+// Import JWT
+import jwt from 'jsonwebtoken';
 
 const auth = (req, res, next) => {
-    const token = req.headers['authorization'];
-    if (!token) return res.status(403).json({ message: 'No token provided' });
+    // Mengambil token dari header Authorization
+    const token = req.headers['authorization']?.split(' ')[1]; // Mengambil token setelah "Bearer"
 
-    jwt.verify(token, 'your_secret_key', (err, decoded) => {
-        if (err) return res.status(500).json({ message: 'Failed to authenticate token' });
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided' });
+    }
+
+    // Memverifikasi token
+    jwt.verify(token, 'abcd1234', (err, decoded) => {
+        if (err) {
+            return res.status(500).json({ message: 'Failed to authenticate token' });
+        }
+        // Menyimpan ID pengguna yang terautentikasi ke dalam request
         req.userId = decoded.id;
         next();
     });
 };
 
-module.exports = auth;
+export default auth;
