e5aeaa3eb685935cd0a196cc1adfead81631677c
Kirasy42 <hanifanaktasik@gmail.com>
2025-01-17 15:33:33 +0700

    feat(api.js): added auth for all endpoint

--- uas-expressJS/routes/api.js
+++ uas-expressJS/routes/api.js
@@ -1,16 +1,21 @@
 import express from 'express';
+import auth from '../middlewares/auth.js';
 import NewsController from '../controllers/NewsController.js';
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    res.send("Welcome to NEWS API");
+});
+
 router.get('/news', NewsController.index);
-router.post('/news', NewsController.store);
-router.put('/news/:id', NewsController.update);
-router.delete('/news/:id', NewsController.destroy);
-router.get('/news/:id', NewsController.show);
-router.get('/news/search/:title', NewsController.search);
-router.get('/news/category/sport', NewsController.sport);
-router.get('/news/category/finance', NewsController.finance);
-router.get('/news/category/automotive', NewsController.automotive);
+router.post('/news', auth, NewsController.store);
+router.put('/news/:id', auth, NewsController.update);
+router.delete('/news/:id', auth, NewsController.destroy);
+router.get('/news/:id', auth, NewsController.show);
+router.get('/news/search/:title', auth, NewsController.search);
+router.get('/news/category/sport', auth, NewsController.sport);
+router.get('/news/category/finance', auth, NewsController.finance);
+router.get('/news/category/automotive', auth, NewsController.automotive);
 
 export default router;
